9a3203e7d9002398195216f1aa20cef65c380bf4
DmytroTarasov <dtarasov24s@gmail.com>
2022-12-11 21:36:38 +0200

    set react strict mode

--- frontend/users_app/src/main.jsx
+++ frontend/users_app/src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	// <React.StrictMode>
+	<React.StrictMode>
 		<App />
-	// </React.StrictMode>
+	</React.StrictMode>
 );
